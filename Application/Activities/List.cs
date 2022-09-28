cefb86942163a76316a934a43d93041a38d57de2
MarioTravanca7 <mtravanca92@gmail.com>
2022-09-28 16:59:15 +0000

    Module 10 (error)

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,11 +13,11 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>>{
+        public class Query : IRequest<Result<List<Activity>>>{
 
         }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
         private readonly DataContext _context;
        
@@ -25,20 +26,9 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                /*try{
-                    for(var i=0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }catch(Exception ex) when (ex is TaskCanceledException){
-                    _logger.LogInformation("Task was cancelled");
-                }*/
-
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
             }
         }
     }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './app/layout/styles.css';
 import 'react-calendar/dist/Calendar.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
