85294f852ece7e417771de0b1ef0db9d24515d1b
MarioTravanca7 <mtravanca92@gmail.com>
2022-10-26 10:06:25 +0000

    End of section 16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,32 +29,39 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.Email == loginDto.Email);
 
-            if(user == null) return Unauthorized();
+            if (user == null) return Unauthorized();
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
-            if(result.Succeeded){
+            if (result.Succeeded)
+            {
                 return CreateUserObject(user);
             }
-            
+
             return Unauthorized();
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
-            if(await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email)){
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
+            {
                 ModelState.AddModelError("email", "Email taken");
                 return ValidationProblem();
             }
-            if(await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName)){
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.UserName))
+            {
                 ModelState.AddModelError("username", "Username taken");
                 return ValidationProblem();
             }
-            
-            var user = new AppUser{
+
+            var user = new AppUser
+            {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
                 UserName = registerDto.UserName
@@ -62,30 +69,33 @@ namespace API.Controllers
 
             var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if(result.Succeeded)
+            if (result.Succeeded)
             {
                 return CreateUserObject(user);
             }
-            
+
             return BadRequest("Problem registering user");
         }
 
-        [Authorize]        
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
 
             return CreateUserObject(user);
         }
 
-        private UserDto CreateUserObject(AppUser user){
-            return new UserDto{
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    Username = user.UserName
-                };
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = user?.Photos?.FirstOrDefault(x => x.IsMain)?.Url,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName
+            };
         }
     }
 }
