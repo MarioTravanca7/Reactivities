5d4afc42628c98769bb76d2fd8115cbbe77eb35d
MarioTravanca7 <mtravanca92@gmail.com>
2022-10-28 11:49:15 +0000

    End of Section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -5,25 +5,25 @@ import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
 import { store } from "./store";
 
-export default class UserStore{
+export default class UserStore {
     user: User | null = null;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    get isLoggedIn(){
+    get isLoggedIn() {
         return !!this.user;
     }
 
-    login = async(creds: UserFormValues) => {
-        try{
+    login = async (creds: UserFormValues) => {
+        try {
             const user = await agent.Account.login(creds);
             store.commonStore.setToken(user.token);
-            runInAction(()=> this.user = user);
+            runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
-        }catch(error){
+        } catch (error) {
             throw error;
         }
     }
@@ -44,14 +44,18 @@ export default class UserStore{
     }
 
     register = async (creds: UserFormValues) => {
-        try{
+        try {
             const user = await agent.Account.register(creds);
             store.commonStore.setToken(user.token);
-            runInAction(()=> this.user = user);
+            runInAction(() => this.user = user);
             history.push('/activities');
             store.modalStore.closeModal();
-        }catch(error){
+        } catch (error) {
             throw error;
         }
+    }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
     }
 }
